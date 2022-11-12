eeea2845de3ea64a42cbd552cdaae601c578ec8e
Seizuree <briangiovanni03@gmail.com>
2022-11-12 15:33:57 +0700

    C during INC

---
+++ INCtrial/caseB.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int res = 0;
+    scanf("%d",&res); getchar();
+    for (int i = 0; i < 100; i++)
+    {
+        for (int j = 0; j < 100; i++)
+        {
+            for (int k = 0; k < 100; i++)
+            {
+                
+            }
+            
+        }
+        
+    }
+    
+}
