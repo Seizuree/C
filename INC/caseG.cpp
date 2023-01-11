17d26a8bed10c76d8420484044e7a57956d14cef
Seizuree <briangiovanni03@gmail.com>
2023-01-11 14:28:29 +0700

    updates on case G

--- INC/caseG.cpp
+++ INC/caseG.cpp
@@ -3,94 +3,94 @@ using namespace std;
 
 int main()
 {
-    long long int n, m;
-    scanf("%lld %lld", &n, &m);
+    long long int p, q;
+    scanf("%lld %lld", &p, &q);
     getchar();
     char a, b;
-    long long int val1, val2;
-    long long int max = m;
-    long long int min = m;
-    for (int i = 0; i < n; i++)
+    long long int x, y;
+    long long int max = q;
+    long long int min = q;
+    for (int i = 0; i < p; i++)
     {
-        long long int maxround = max;
-        long long int minround = min;
-        scanf("%c %lld %c %lld", &a, &val1, &b, &val2);
+        long long int maxcalc = max;
+        long long int mincalc = min;
+        scanf("%c %lld %c %lld", &a, &x, &b, &y);
         getchar();
         if (a == 'x')
         {
-            long long int temp1 = val1 * max;
-            maxround = temp1;
-            minround = temp1;
-            long long int temp2 = val1 * min;
-            if (maxround < temp2)
+            long long int temp1 = x * max;
+            maxcalc = temp1;
+            mincalc = temp1;
+            long long int temp2 = x * min;
+            if (maxcalc < temp2)
             {
-                maxround = temp2;
+                maxcalc = temp2;
             }
-            if (minround > temp2)
+            if (mincalc > temp2)
             {
-                minround = temp2;
+                mincalc = temp2;
             }
         }
         else if (a == '+')
         {
-            long long int temp1 = val1 + max;
-            maxround = temp1;
-            minround = temp1;
-            long long int temp2 = val1 + min;
-            if (maxround < temp2)
+            long long int temp1 = x + max;
+            maxcalc = temp1;
+            mincalc = temp1;
+            long long int temp2 = x + min;
+            if (maxcalc < temp2)
             {
                 max = temp2;
             }
-            if (minround > temp2)
+            if (mincalc > temp2)
             {
-                minround = temp2;
+                mincalc = temp2;
             }
         }
 
         if (b == 'x')
         {
-            long long int temp1 = val2 * max;
-            if (maxround < temp1)
+            long long int temp1 = y * max;
+            if (maxcalc < temp1)
             {
-                maxround = temp1;
+                maxcalc = temp1;
             }
-            if (minround > temp1)
+            if (mincalc > temp1)
             {
-                minround = temp1;
+                mincalc = temp1;
             }
-            long long int temp2 = val2 * min;
-            if (maxround < temp2)
+            long long int temp2 = y * min;
+            if (maxcalc < temp2)
             {
-                maxround = temp2;
+                maxcalc = temp2;
             }
-            if (minround > temp2)
+            if (mincalc > temp2)
             {
-                minround = temp2;
+                mincalc = temp2;
             }
         }
         else if (b == '+')
         {
-            long long int temp1 = val2 + max;
-            if (maxround < temp1)
+            long long int temp1 = y + max;
+            if (maxcalc < temp1)
             {
-                maxround = temp1;
+                maxcalc = temp1;
             }
-            if (minround > temp1)
+            if (mincalc > temp1)
             {
-                minround = temp1;
+                mincalc = temp1;
             }
-            long long int temp2 = val2 + min;
-            if (maxround < temp2)
+            long long int temp2 = y + min;
+            if (maxcalc < temp2)
             {
-                maxround = temp2;
+                maxcalc = temp2;
             }
-            if (minround > temp2)
+            if (mincalc > temp2)
             {
-                minround = temp2;
+                mincalc = temp2;
             }
         }
-        max = maxround;
-        min = minround;
+        max = maxcalc;
+        min = mincalc;
     }
     printf("%lld\n", max);
     return 0;
